3266608522bce8c3dbdaf7819b413551e64fad5b
unknown <namithadsouza59@gmail.com>
2026-06-21 19:51:54 +0100

    feat: Implemented join_room socket event

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,15 @@ const PORT = process.env.PORT || 4000;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('join_room', (roomId) => {
+    socket.join(roomId)
+    console.log(`${socket.data.user.email} joined room ${roomId}`)
+
+    socket.to(roomId).emit('user_joined', {
+      userId: socket.data.user.id,
+      email: socket.data.user.email
+    })
+  })
 });
 
 server.listen(PORT, () => {
