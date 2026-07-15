ac6bed6f8a8003c683e17cb94f8a1dae38ee5f1f
unknown <namithadsouza59@gmail.com>
2026-07-15 12:58:38 +0100

    fix: add prisma generate to build step

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -2,10 +2,8 @@ import { io } from 'socket.io-client'
 
 const token = localStorage.getItem('token')
 
-const socket = io('http://localhost:4000', {
-  auth: {
-    token: token    
-  }
+const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:4000', {
+  auth: { token }
 })
 
 export default socket
